70cb517e93d454035f24708de11515010c2ff588
IsmaylHossain <ismaylhossain786@gmail.com>
2023-10-03 21:10:09 +0600

    tatics page has created

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -9,14 +9,10 @@
 //          <div className="mx-auto card w-72 bg-base-100 shadow-xl">
 // <figure><img className="w-72" src={picture} alt="Shoes" /></figure>
 // <div className="card-body rounded-b-2xl" style={{ backgroundColor: card_bg }}>
-// <p className=" w-20 pl-2 rounded-md" style={{ backgroundColor: category_bg, color: text_bg}}>{category}</p>
+// <p className=" w-20  rounded-md" style={{ backgroundColor: category_bg, color: text_bg}}>{category}</p>
 //   <h2 className="card-title w-60" style={{ color: text_bg}} >{title}</h2>
 // </div>
 // </div>
-
-
-
- 
 //       </div>
 //   );
 // };
@@ -32,7 +28,7 @@ const Card = ({card}) => {
 const {id,image,color,te_color, comp_color,  components, title}= card || {}
 
 const textStyle = {
-backgroundColor: color // Use the card_bg value as the background color
+backgroundColor: color  
 };
 const backStyle = {
 color: te_color
@@ -44,11 +40,11 @@ backgroundColor: comp_color
 return (
 <div>
 <Link to={`/cards/${id}`}>
-<div className="card w-70 bg-base-100 shadow-xl" style={textStyle}>
-<figure><img src={image} alt="Shoes" /></figure>
-<div className="card-body">
-<p style={compoStyle} className="w-20 h-9 rounded-md text-center">{components}</p>
-<h2 className="card-title" style={backStyle}>{title}</h2>
+<div className="mx-auto card w-72 bg-base-100 shadow-xl" style={textStyle}>
+<figure><img className="w-72" src={image} alt="Shoes" /></figure>
+<div className="card-body rounded-b-2xl">
+<p style={compoStyle} className="w-20  rounded-md text-center">{components}</p>
+<h2 className="card-title w-72" style={backStyle}>{title}</h2>
 </div>
 </div>
 </Link>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@
 const Header = () => {
     return (
         <div className="flex flex-col justify-center items-center gap-10 h-[70vh] bg-green-200">
-            <h1 className="text-4xl font-bold">I Grow By Helping People in Need</h1>
+            <h1 className="text-4xl font-bold mx-auto text-center">I Grow By Helping People in Need</h1>
             <div className="flex">
                 <input type="text" placeholder="Type here" className="input input-bordered input-md w-full max-w-xs rounded-r-none" />
                 <button className="btn btn-error rounded-l-none text-white">Search</button>
