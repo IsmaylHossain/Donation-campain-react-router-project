e2c62974e29eb9ba8b1d5618dd835593d6ecab33
IsmaylHossain <ismaylhossain786@gmail.com>
2023-10-07 13:09:12 +0600

    complete the projects

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,4 @@
 
-
-// const Header = () => {
-//     return (
-//         <div className="flex  background-img flex-col justify-center  items-center gap-10 h-[70vh] bg-green-200">
-        
-//             <h1 className="text-4xl font-bold mx-auto text-center">I Grow By Helping People in Need</h1>
-//             <div className="flex">
-//                 <input type="text" placeholder="Type here" className="input input-bordered input-md w-full max-w-xs rounded-r-none" />
-//                 <button className="btn btn-error rounded-l-none text-white">Search</button>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Header;
-
 import { useState } from "react";
 import PropTypes from 'prop-types';
 
@@ -35,7 +19,7 @@ const Header = ({handleSearch}) => {
         <div className="relative">
             <div className="top-0 -z-50">
                 <img className="w-screen" src="https://i.ibb.co/qMrStTZ/Screenshot-2023-09-26-193322.png" alt="Banner"/>
-                    <div className="absolute inset-0 flex flex-col items-center  bg-white bg-opacity-[80%]   md:justify-center ">
+                    <div className="absolute inset-0 bg-white bg-opacity-[95%] flex flex-col items-center   md:justify-center ">
                         <h1 className="text-black text-xl md:text-2xl lg:text-5xl font-bold p-5">I Grow By Helping People In Need</h1>
                         <div className="join">
                             <input onChange={handleInput} value={search} className="input input-bordered join-item bg-white" name="searchInput" placeholder="Search here"/>
